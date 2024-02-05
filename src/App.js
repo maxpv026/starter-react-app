b8be1eb866fcdb553a85e4c11418e75a3fcc671a
Maksym Pyvovarov <119981087+maxpv026@users.noreply.github.com>
2024-02-06 01:21:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +22,3 @@ function App() {
   );
 }
 export default App;
-
